8051b0b571241f04bea29594c9531aa63e940b7a
3strategy <3strategy@gmail.com>
2025-04-21 15:31:55 +0300

    another mermaid attempt

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,8 +1,3 @@
-
-import {mermaid} from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
-mermaid.initialize({ startOnLoad: true });
-
-
 // Cookie helper functions
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
